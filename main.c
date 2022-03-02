10660d9c56d8499709c7683bf540f7adbb1613a4
Karitchi <karichikarichikarichi@gmail.com>
2022-03-02 14:48:30 +0100

    change *char boxes[] to char boxes[][] (this make the array writable)

--- main.c
+++ main.c
@@ -1,53 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void printBoard(char boxes[3][3])
 {
     system("clear");
     printf("         |         |          \n");
     printf("         |         |          \n");
-    printf("    %c    |    %c    |    %c    \n", boxes[0], boxes[1], boxes[2]);
+    printf("    %c    |    %c    |    %c    \n", boxes[0][0], boxes[0][1], boxes[0][2]);
     printf("         |         |          \n");
     printf("_________|_________|__________\n");
     printf("         |         |          \n");
     printf("         |         |          \n");
-    printf("    %c    |    %c    |    %c    \n", boxes[3], boxes[4], boxes[5]);
+    printf("    %c    |    %c    |    %c    \n", boxes[1][0], boxes[1][1], boxes[1][2]);
     printf("         |         |          \n");
     printf("_________|_________|__________\n");
     printf("         |         |          \n");
     printf("         |         |          \n");
-    printf("    %c    |    %c    |    %c    \n", boxes[6], boxes[7], boxes[8]);
+    printf("    %c    |    %c    |    %c    \n", boxes[2][0], boxes[2][1], boxes[2][2]);
     printf("         |         |          \n");
     printf("         |         |          \n");
 }
 
-void askPlayerBox(char boxes[], int *player, int *chosenBox)
+void askPlayerBox(char boxes[3][3], int *player, int *x, int *y)
 {
 
     do
     {
         printf("Player %d, chose a box to play on: ", *player);
-        scanf("%d", chosenBox);
-    } while (boxes[*chosenBox - 1] == 'O' || boxes[*chosenBox - 1] == 'X');
+        scanf("%d", &chosenBox);
+        *x = (chosenBox - 1) % 3;
+        *y = (chosenBox - 1) / 3;
+    } while (boxes[*y][*x] == 'O' || boxes[*y][*x] == 'X' || chosenBox < 1 || chosenBox > 9);
 }
 
-void storePlay(char boxes[], int *player, int chosenBox)
+void storePlay(char boxes[3][3], int *player, int *x, int *y)
 {
     if (*player)
     {
-        boxes[chosenBox - 1] = 'O';
+        boxes[*y][*x] = 'O';
         *player = *player - 1;
     }
     else
     {
-        boxes[chosenBox - 1] = 'X';
+        boxes[*y][*x] = 'X';
         *player = *player + 1;
     }
 }
 
 void main(void)
 {
-    char boxes[] = {"123456789"};
-    int chosenBox;
+    int x, y, chosenBox;
+    char boxes[3][3] = {"123", "456", "789"};
     int player = 0;
     int isFinished = 9;
 
